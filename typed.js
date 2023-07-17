89224febac732b4dd8172f178470a96574e58ddc
0Sacha <113936136+0Sacha@users.noreply.github.com>
2023-07-17 17:36:12 +0200

    Change interface design V 0.0.3

---
+++ typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    Strings: ["Fontend Developer", "Designer", "Graphiste"],
+    typeSpeed: 100,
+    bakSpeed: 100,
+    bakDelay: 1000,
+    loop: true
+})
