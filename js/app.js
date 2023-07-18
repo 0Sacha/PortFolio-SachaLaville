d8dfb8fa8419ea84427a16c8f5d56e4fd08ae6c0
0Sacha <113936136+0Sacha@users.noreply.github.com>
2023-07-18 16:24:37 +0200

    change design interface V 0.0.6

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-
-darkModeToggle.addEventListener('click', () => {
-  const body = document.querySelector('body');
-  body.classList.toggle('dark-mode');
-});
