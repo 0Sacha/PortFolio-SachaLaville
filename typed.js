33d839cc98e9a51c92908428663cf1b799f02bae
0Sacha <113936136+0Sacha@users.noreply.github.com>
2023-07-18 20:08:30 +0200

    Add Images V0.0.2

--- typed.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text", {
-    Strings: ["Fontend Developer", "Designer", "Graphiste"],
-    typeSpeed: 100,
-    bakSpeed: 100,
-    bakDelay: 1000,
-    loop: true
-})
