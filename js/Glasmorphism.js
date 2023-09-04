4c0a512c3c54346e29f394a658cc6d8facf835c6
0Sacha <113936136+0Sacha@users.noreply.github.com>
2023-09-04 11:07:06 +0200

    ré-organisation des fichiers

---
+++ js/Glasmorphism.js
@@ -0,0 +1,14 @@
+VanillaTilt.init(document.querySelectorAll(".card-logomc"), {
+                max: 25,
+                speed: 400
+            });
+
+VanillaTilt.init(document.querySelectorAll(".card-Kideo"), {
+                max: 25,
+                speed: 400
+            });
+
+VanillaTilt.init(document.querySelectorAll(".card-logoEtna"), {
+                max: 25,
+                speed: 400
+            })
